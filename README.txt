27d91dd62d96a2ff2a6803fe3157bc68476f5d18
miura <16eo00127@gmail.com>
2023-10-21 16:34:38 +0900

    step1実装とテストコード実装

---
+++ README.txt
@@ -0,0 +1,8 @@
+カバレッジレポート作成コマンド
+```
+"CODE_DIRECTORY"はテストしたいファイルのディレクトリを指定
+```
+pytest -v --cov=CODE_DIRECTORY --cov-report=html
+
+
+
--- src/VendingMachine.py
+++ src/VendingMachine.py
@@ -1,4 +1,14 @@
-#step1 自販機お金の処理
+# step1 自販機お金の処理
+MENU_PROMPT = (
+    "メニュー:\n"
+    "1. お金を投入します。\n"
+    "2. 総計を確認します。\n"
+    "3. 払い戻しをします。\n"
+    "4. 終了します。\n"
+    "選択肢を入力してください。: "
+)
+
+
 class VendingMachine:
     # 投入金額の総計
     def __init__(self):
@@ -6,15 +16,12 @@ class VendingMachine:
 
     # 10円玉、50円玉、100円玉、500円玉、1000円札を受け付ける
     def insert_coin_or_payout(self, amount_integer):
-        if not isinstance(amount_integer, int):
-            return "無効な入力です。お金を入れてください。"
-        
         if amount_integer in [10, 50, 100, 500, 1000]:
             self.total_entry_amount += amount_integer
             return f"{amount_integer}円を投入しました。"
         else:
-            return "無効な値です。"
-            
+            return "無効なお金です。"
+
     # 投入金額の総計を取得
     def get_total_amount(self):
         return self.total_entry_amount
@@ -28,36 +35,30 @@ class VendingMachine:
         else:
             return "釣り銭はありません。"
 
-    def isinteger_insert(amount):
-        try:
-            value = int(input(amount))
-            return value
-        except ValueError:
-            return "無効な入力です。お金を入れてください。"
 
 def isinteger_insert(amount):
     try:
         return int(amount)
     except ValueError:
-        return "無効な入力です。お金を入れてください。"
+        return None
+
 
 def main():
     # 自販機を作成
     creation_vending_machine = VendingMachine()
 
     while True:
-        print("メニュー:")
-        print("1. お金を投入します。")
-        print("2. 総計を確認します。")
-        print("3. 払い戻しをします。")
-        print("4. 終了します。")
-
-        selection = input("選択肢を入力してください。: ")
+        print(MENU_PROMPT, end="")
+        selection = input()
 
         if selection == "1":
-            amount = input("投入するお金を入力してください。: ")
+            amount = input("お金を入れてください。: ")
             amount_integer = isinteger_insert(amount)
-            print(creation_vending_machine.insert_coin_or_payout(amount_integer))
+
+            if amount_integer is None:
+                print("無効な入力です。お金を入れてください。")
+            else:
+                print(creation_vending_machine.insert_coin_or_payout(amount_integer))
         elif selection == "2":
             total_entry_amount = creation_vending_machine.get_total_amount()
             print(f"投入金額の総計は{total_entry_amount}円です。")
@@ -70,5 +71,6 @@ def main():
             print("無効な選択肢です。")
         print("\n")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":  # pragma: no cover
     main()
