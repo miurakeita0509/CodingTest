c417412f3e73a77b48adaaef16c273cab51e3c31
miura <16eo00127@gmail.com>
2023-10-23 17:58:08 +0900

    step3の実装とテストコードの実装

--- test/test_JuiceManagement.py
+++ test/test_JuiceManagement.py
@@ -6,5 +6,24 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent))
 sys.path.append(str(Path(__file__).parent.parent / "src"))
-from src.VendingMachine import VendingMachine, isinteger_insert
-from src.main import main, MENU_PROMPT
+from src.JuiceManagement import JuiceManagement
+
+
+def test_init_normal():
+    jm = JuiceManagement()
+    expected_juices = {
+        "cola": {"name": "コーラ", "price": 120, "stock": 5},
+    }
+    assert jm.get_juice_info() == expected_juices
+
+
+@pytest.mark.parametrize(
+    ("juice", "expected_value"),
+    [
+        ("cola", {"name": "コーラ", "price": 120, "stock": 5}),
+        ("test", None),
+    ],
+)
+def test_get_juice_info(juice, expected_value):
+    jm = JuiceManagement()
+    assert jm.get_juice_info(juice) == expected_value
