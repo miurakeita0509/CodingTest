c48dc378cf5c17dfd6df1c5b4d8fc39ea363c2c1
miura <16eo00127@gmail.com>
2023-10-30 21:46:54 +0900

    main.pyとjuicemanagement.pyのテストコード作成 READMEの編集

--- test/test_main.py
+++ test/test_main.py
@@ -8,6 +8,7 @@ sys.path.append(str(Path(__file__).parent.parent))
 sys.path.append(str(Path(__file__).parent.parent / "src"))
 from src.VendingMachine import VendingMachine, isinteger_insert
 from src.main import main, MENU_PROMPT
+from src.JuiceManagement import JuiceManagement
 
 
 # UIのテストコード
@@ -15,12 +16,12 @@ from src.main import main, MENU_PROMPT
     ("input_str", "exception_output"),
     [
         (
-            "1\n" "50\n" "4\n",
+            "1\n" "50\n" "6\n",
             MENU_PROMPT + "お金を入れてください。: 50円を投入しました。\n"
             "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
         ),
         (
-            "1\n" "a\n" "4\n",
+            "1\n" "a\n" "6\n",
             MENU_PROMPT + "お金を入れてください。: 無効な入力です。お金を入れてください。\n"
             "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
         ),
@@ -39,35 +40,35 @@ def test_main_selection1_normal(capsys, monkeypatch, input_str, exception_output
     ("input_str", "exception_output"),
     [
         (
-            "2\n" "4\n",
+            "2\n" "6\n",
             MENU_PROMPT + "投入金額の総計は0円です。\n" "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
         ),
         (
-            "1\n" "10\n" "2\n" "4\n",
+            "1\n" "10\n" "2\n" "6\n",
             MENU_PROMPT + "お金を入れてください。: 10円を投入しました。\n"
             "\n\n" + MENU_PROMPT + "投入金額の総計は10円です。\n"
             "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
         ),
         (
-            "1\n" "50\n" "2\n" "4\n",
+            "1\n" "50\n" "2\n" "6\n",
             MENU_PROMPT + "お金を入れてください。: 50円を投入しました。\n"
             "\n\n" + MENU_PROMPT + "投入金額の総計は50円です。\n"
             "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
         ),
         (
-            "1\n" "100\n" "2\n" "4\n",
+            "1\n" "100\n" "2\n" "6\n",
             MENU_PROMPT + "お金を入れてください。: 100円を投入しました。\n"
             "\n\n" + MENU_PROMPT + "投入金額の総計は100円です。\n"
             "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
         ),
         (
-            "1\n" "500\n" "2\n" "4\n",
+            "1\n" "500\n" "2\n" "6\n",
             MENU_PROMPT + "お金を入れてください。: 500円を投入しました。\n"
             "\n\n" + MENU_PROMPT + "投入金額の総計は500円です。\n"
             "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
         ),
         (
-            "1\n" "1000\n" "2\n" "4\n",
+            "1\n" "1000\n" "2\n" "6\n",
             MENU_PROMPT + "お金を入れてください。: 1000円を投入しました。\n"
             "\n\n" + MENU_PROMPT + "投入金額の総計は1000円です。\n"
             "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
@@ -87,13 +88,37 @@ def test_main_selection2_normal(capsys, monkeypatch, input_str, exception_output
     ("input_str", "exception_output"),
     [
         (
-            "3\n" "4\n",
+            "3\n" "6\n",
             MENU_PROMPT + "釣り銭はありません。\n" "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
         ),
         (
-            "1\n" "10\n" "3\n" "4\n",
+            "1\n" "10\n" "3\n" "6\n",
             MENU_PROMPT + "お金を入れてください。: 10円を投入しました。\n"
             "\n\n" + MENU_PROMPT + "10円の釣り銭を返金します。\n"
+            "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
+        ),
+        (
+            "1\n" "50\n" "3\n" "6\n",
+            MENU_PROMPT + "お金を入れてください。: 50円を投入しました。\n"
+            "\n\n" + MENU_PROMPT + "50円の釣り銭を返金します。\n"
+            "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
+        ),
+        (
+            "1\n" "100\n" "3\n" "6\n",
+            MENU_PROMPT + "お金を入れてください。: 100円を投入しました。\n"
+            "\n\n" + MENU_PROMPT + "100円の釣り銭を返金します。\n"
+            "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
+        ),
+        (
+            "1\n" "500\n" "3\n" "6\n",
+            MENU_PROMPT + "お金を入れてください。: 500円を投入しました。\n"
+            "\n\n" + MENU_PROMPT + "500円の釣り銭を返金します。\n"
+            "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
+        ),
+        (
+            "1\n" "1000\n" "3\n" "6\n",
+            MENU_PROMPT + "お金を入れてください。: 1000円を投入しました。\n"
+            "\n\n" + MENU_PROMPT + "1000円の釣り銭を返金します。\n"
             "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
         ),
     ],
@@ -111,24 +136,164 @@ def test_main_selection3_normal(capsys, monkeypatch, input_str, exception_output
     ("input_str", "exception_output"),
     [
         (
-            "0\n" "4\n",
+            "0\n" "6\n",
             MENU_PROMPT + "無効な選択肢です。\n" "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
         ),
         (
-            "a\n" "4\n",
+            "a\n" "6\n",
             MENU_PROMPT + "無効な選択肢です。\n" "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
         ),
         (
-            "'''\n" "4\n",
+            "'''\n" "6\n",
             MENU_PROMPT + "無効な選択肢です。\n" "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
         ),
         (
-            " \n" "4\n",
+            " \n" "6\n",
             MENU_PROMPT + "無効な選択肢です。\n" "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
         ),
     ],
 )
-def test_main_selection_normal(capsys, monkeypatch, input_str, exception_output):
+def test_main_selection_abnormal(capsys, monkeypatch, input_str, exception_output):
+    monkeypatch.setattr("sys.stdin", io.StringIO(input_str))
+    main()
+    # 標準出力のキャプチャを取得
+    captured = capsys.readouterr()
+
+    assert captured.out == exception_output
+
+
+@pytest.mark.parametrize(
+    ("input_str", "exception_output"),
+    [
+        (
+            "1\n" "1000\n" "4\n" "1\n" "6\n",
+            MENU_PROMPT + "お金を入れてください。: 1000円を投入しました。\n"
+            "\n\n"
+            + MENU_PROMPT
+            + "\n\n"
+            + "0. 購入をやめますか？\n"
+            + "1.コーラ:購入可能です。\n"
+            + "どのドリンクを購入しますか。: コーラを購入しました。\n"
+            "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
+        ),
+        (
+            "4\n" "0\n" "6\n",
+            MENU_PROMPT
+            + "\n\n"
+            + "0. 購入をやめますか？\n"
+            + "1.コーラ:投入金額が足りません。\n"
+            + "どのドリンクを購入しますか。: 購入をやめます。\n"
+            "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
+        ),
+        (
+            "4\n" "a\n" "0\n" "6\n",
+            MENU_PROMPT
+            + "\n\n"
+            + "0. 購入をやめますか？\n"
+            + "1.コーラ:投入金額が足りません。\n"
+            + "どのドリンクを購入しますか。: 無効な選択です。再度選択してください。\n"
+            "\n\n"
+            + "0. 購入をやめますか？\n"
+            + "1.コーラ:投入金額が足りません。\n"
+            + "どのドリンクを購入しますか。: 購入をやめます。\n"
+            "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
+        ),
+    ],
+)
+def test_main_selection4_normal(capsys, monkeypatch, input_str, exception_output):
+    monkeypatch.setattr("sys.stdin", io.StringIO(input_str))
+    main()
+
+    # 標準出力のキャプチャを取得
+    captured = capsys.readouterr()
+
+    assert captured.out == exception_output
+
+
+class MockJuiceManagement:
+    def __init__(self):
+        self.juice = {
+            "コーラ": {"name": "コーラ", "price": 120, "stock": 0},
+        }
+
+    def show_juice_menu(self, vending_machine):
+        index_name_map = {
+            1: "コーラ",
+        }
+        return index_name_map
+
+    def can_purchase(self, amount, juice_name):
+        amount = 150
+        juice_name = "コーラ"
+        return (amount >= self.juice[juice_name]["price"]) and (
+            self.juice[juice_name]["stock"] > 0
+        )
+
+
+@pytest.mark.parametrize(
+    ("input_str", "exception_output"),
+    [
+        (
+            "1\n" "1000\n" "4\n" "1\n" "0\n" "6\n",
+            MENU_PROMPT + "お金を入れてください。: 1000円を投入しました。\n"
+            "\n\n" + MENU_PROMPT + "\n\n" + "どのドリンクを購入しますか。: コーラは購入できません。再度選択してください。\n"
+            "\n\n" + "どのドリンクを購入しますか。: 購入をやめます。\n"
+            "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
+        ),
+    ],
+)
+def test_main_selection4_stock0_normal(
+    capsys, monkeypatch, input_str, exception_output
+):
+    monkeypatch.setattr("src.main.JuiceManagement", MockJuiceManagement)
+    monkeypatch.setattr("sys.stdin", io.StringIO(input_str))
+    main()
+    # 標準出力のキャプチャを取得
+    captured = capsys.readouterr()
+
+    assert captured.out == exception_output
+
+
+@pytest.mark.parametrize(
+    ("input_str", "exception_output"),
+    [
+        (
+            "5\n" "6\n",
+            MENU_PROMPT + "現在の売上は0円です。\n" "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
+        ),
+        (
+            "1\n" "1000\n" "4\n" "1\n" "5\n" "6\n",
+            MENU_PROMPT + "お金を入れてください。: 1000円を投入しました。\n"
+            "\n\n"
+            + MENU_PROMPT
+            + "\n\n"
+            + "0. 購入をやめますか？\n"
+            + "1.コーラ:購入可能です。\n"
+            + "どのドリンクを購入しますか。: コーラを購入しました。\n"
+            "\n\n" + MENU_PROMPT + "現在の売上は120円です。\n"
+            "\n\n" + MENU_PROMPT + "自販機を終了します。\n",
+        ),
+    ],
+)
+def test_main_selection5_normal(capsys, monkeypatch, input_str, exception_output):
+    monkeypatch.setattr("sys.stdin", io.StringIO(input_str))
+    main()
+    # 標準出力のキャプチャを取得
+    captured = capsys.readouterr()
+
+    assert captured.out == exception_output
+
+
+@pytest.mark.parametrize(
+    ("input_str", "exception_output"),
+    [
+        (
+            "6\n",
+            MENU_PROMPT + "自販機を終了します。\n",
+        ),
+    ],
+)
+def test_main_selection6_normal(capsys, monkeypatch, input_str, exception_output):
     monkeypatch.setattr("sys.stdin", io.StringIO(input_str))
     main()
     # 標準出力のキャプチャを取得
